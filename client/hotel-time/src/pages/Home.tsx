72fd871e20806931638d72bac4cde4419695c0d5
leulmesfin116 <leul116hga@gmail.com>
2026-06-25 16:56:14 +0300

    home page

--- client/hotel-time/src/pages/Home.tsx
+++ client/hotel-time/src/pages/Home.tsx
@@ -1,5 +1,17 @@
 function Home() {
 return(<>
-<h1>Hotel Time </h1></>)
+<header><h1 className="text-4xl font-serif text-amber-500 ">Hotel Time </h1></header>
+<div className="flex flex-col m-4">
+ <h2 className="font-bold text-xl font-serif" >Hotel time ,Dessie</h2> 
+ <a 
+  href="https://www.google.com/maps/search/?api=1&query=4JVV%2BP5Q%2C%201%2C%20Dese" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="underline"
+>4JVV+P5Q, 1, Dese
+</a>
+</div>
+</> )
+
 }
 export  default Home;
