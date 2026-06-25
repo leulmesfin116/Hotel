fab669d705b651674b9ba998e959c1083d89f07b
leulmesfin116 <leul116hga@gmail.com>
2026-06-25 18:20:27 +0300

    home page

--- client/hotel-time/src/pages/Home.tsx
+++ client/hotel-time/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 function Home() {
 return(<>
-<header><h1 className="text-4xl font-serif text-amber-500 ">Hotel Time </h1></header>
+<header className="m-3"><h1 className="text-4xl font-serif text-amber-500 ">Hotel Time </h1></header>
+<hr></hr>
+<div>
 <div className="flex flex-col m-4">
  <h2 className="font-bold text-xl font-serif" >Hotel time ,Dessie</h2> 
  <a 
@@ -11,7 +13,11 @@ return(<>
 >4JVV+P5Q, 1, Dese
 </a>
 </div>
+<hr></hr>
+<div></div>
+</div>
 </> )
+
 
 }
 export  default Home;
