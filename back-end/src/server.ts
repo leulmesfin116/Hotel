74227f9fd1fe090a9713cc2872b0c560cd2b11f9
leulmesfin116 <leul116hga@gmail.com>
2026-07-05 19:44:07 +0300

    updating

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -7,15 +7,3 @@ const app = express();
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.use(express.json());
-
-app.get("/health", (_req: Request, res: Response) => {
-  res.json({ status: "ok", uptime: process.uptime() });
-});
-
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hotel backend server is running");
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
