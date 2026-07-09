411de8eeb2827ddaf14eafef4ad8fb73958f13f4
leulmesfin116 <leul116hga@gmail.com>
2026-07-09 15:51:05 +0300

    fetching the availiable room  functionality

--- back-end/src/controller/roomController.ts
+++ back-end/src/controller/roomController.ts
@@ -1,5 +1,22 @@
 import { prisma } from '../config/db';
 import { Request, Response } from 'express';
 
-const room = async (req: Request, res: Response) => {};
+const room = async (req: Request, res: Response) => {
+  try {
+    const availableRooms = await prisma.room.findMany({
+      where: { is_avaliable: true },
+      select: { id: true },
+    });
+
+    const roomIds = availableRooms.map((room) => room.id);
+
+    res.status(200).json({
+      success: true,
+      data: roomIds,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+};
+
 export { room };
