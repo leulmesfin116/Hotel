5da007095328bca84c6d54d47eec29a34916d741
leulmesfin116 <leul116hga@gmail.com>
2026-07-21 16:03:31 +0300

    cache invalidation

--- back-end/src/controller/bookingController.ts
+++ back-end/src/controller/bookingController.ts
@@ -1,3 +1,25 @@
 import { prisma } from '../config/db';
 import { Request, Response } from 'express';
 import { redisClient } from '../config/redisClient';
+
+export const bookRoom = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { roomId } = req.body;
+  const updateRoom = await prisma.room.update({
+    where: { id: roomId },
+    data: { is_avaliable: false },
+  });
+  try{
+    const keys =redisClient.keys('rooms:*')
+    if(keys.length>0){
+     await redisClient.del(keys)
+     console.log("")
+    }
+
+  }
+  catch(){
+
+  }
+};
