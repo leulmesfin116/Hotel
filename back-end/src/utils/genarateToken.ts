cfe05b0cd5d6d2ba2b94142b67498e08ecf6d052
leulmesfin116 <leul116hga@gmail.com>
2026-07-26 14:51:44 +0300

    updating

--- back-end/src/utils/genarateToken.ts
+++ back-end/src/utils/genarateToken.ts
@@ -2,12 +2,13 @@ import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 export const generateToken = (userId: string, res: Response) => {
   const payload = { user: userId };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+  const token = jwt.sign(payload, process.env.JWT_SECRET as string, {
+    expiresIn: (process.env.JWT_EXPIRES_IN || '7d') as any,
   });
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
