d9404932dce1addb9d100935c050dc82a036749d
leulmesfin116 <leul116hga@gmail.com>
2026-07-26 12:39:29 +0300

    updating

--- back-end/src/controller/authController.ts
+++ back-end/src/controller/authController.ts
@@ -1,8 +1,9 @@
 import { prisma } from '../config/db';
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
+import { generateToken } from '../utils/genarateToken';
 const register = async (req: Request, res: Response) => {
-  const { full_name, email, password,phone_number } = req.body;
+  const { full_name, email, password, phone_number } = req.body;
 
   // checking if the user already exits
   const userExists = await prisma.user.findFirst({
@@ -16,7 +17,7 @@ const register = async (req: Request, res: Response) => {
   const hashPassword = await bcrypt.hash(password, salt);
   //  creating a user
   const user = await prisma.user.create({
-    data: { full_name, email, password: hashPassword,phone_number} as any, // bypassing typechecking for password since it's missing in schema, or they might add it
+    data: { full_name, email, password: hashPassword, phone_number } as any, // bypassing typechecking for password since it's missing in schema, or they might add it
   });
   res.status(201).json({
     status: 'success',
@@ -24,14 +25,14 @@ const register = async (req: Request, res: Response) => {
       id: user.id,
       email: email,
       full_name: full_name,
-      phone_number:phone_number,
-      password:hashPassword
+      phone_number: phone_number,
+      password: hashPassword,
     },
   });
 };
 const login = async (req: Request, res: Response) => {
   const { password, email } = req.body;
-  const user = await prisma.user.findMany({
+  const user = await prisma.user.findUnique({
     where: { email: email },
   });
   if (!user) {
@@ -43,9 +44,10 @@ const login = async (req: Request, res: Response) => {
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
     res.status(401).json({
-      message:"invalid password or email"
-    })
+      message: 'invalid password or email',
+    });
   }
+  const token = generateToken(user.id);
 };
 
 export { register, login };
