fa69991a14dcab9e73683fdc7315375c42729a55
leulmesfin116 <leul116hga@gmail.com>
2026-07-26 14:46:45 +0300

    cookie

--- back-end/src/utils/genarateToken.ts
+++ back-end/src/utils/genarateToken.ts
@@ -1,9 +1,14 @@
 import jwt from 'jsonwebtoken';
-
-export const generateToken = (userId: string) => {
+import { Request, Response } from 'express';
+export const generateToken = (userId: string, res: Response) => {
   const payload = { user: userId };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+  });
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
 };
