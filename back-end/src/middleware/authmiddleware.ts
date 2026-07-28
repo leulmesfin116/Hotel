9372183dbfa448588876788fa486326b1312eece
leulmesfin116 <leul116hga@gmail.com>
2026-07-28 10:04:17 +0300

    middleware

--- back-end/src/middleware/authmiddleware.ts
+++ back-end/src/middleware/authmiddleware.ts
@@ -6,4 +6,19 @@ export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split('')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  } else {
+    !token;
+  }
+  res.status(401).json({
+    message: 'user is not authorized',
+  });
+};
